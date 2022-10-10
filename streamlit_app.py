af87fad530b3574ad4516b36a5751e870e8fcf5b
thibautcr <94442707+thibautcr@users.noreply.github.com>
2022-10-10 17:53:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+import requests as re
+import json
+import matplotlib.pyplot as plt
 
+st.set_page_config(page_title="App détection billet")
+
+st.title("Application de détection des faux billets")
