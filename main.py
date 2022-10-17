f3d6e9851dce29e828175002cfe794b27774f9e2
thibautcr <94442707+thibautcr@users.noreply.github.com>
2022-10-17 17:20:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ data_set = st.container()
 features = st.container()
 modelTraining = st.container()
 
-st.set_page_config(page_title="App détection billet")
+# st.set_page_config(page_title="App détection billet")
 
-st.title("Application de détection des faux billets")
+# st.title("Application de détection des faux billets")
 
 with header: 
 	st.title('application web détection')
+	
+st.markdown("""<style>.streamlit-expanderHeader{font-size: 17px;}</style>
+<div style="text-align: justify;">Cette application Streamlit, développée dans le cadre du projet 10 de la formation Data Analyst v2 d'OpenClassrooms, utilise un modèle d'apprentissage supervisé de classification (Régression logistique) servant d'API afin de détecter les billets frauduleux en fonction de leurs dimensions.</div>""", unsafe_allow_html=True)
+st.write("""Vous pouvez:  
+- Rentrer manuellement les dimensions d'un billet dans la bar sur le côté.  
+- Charger un fichier *csv* qui devra être encodé en utf-8. Le fichier doit ressembler à cela et les colonnes doivent correspondre :""")
+df = pd.read_csv("billets_production.csv")
