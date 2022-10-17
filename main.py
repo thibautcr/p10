124b1b54a757bdd9d62141ec5d3e15d8428104ab
thibautcr <94442707+thibautcr@users.noreply.github.com>
2022-10-17 17:03:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import requests as re
+import json
+import matplotlib.pyplot as plt
+
+
+header = st.beta_container()
+data_set = st.beta_container()
+features = st.beta_container()
+modelTraining = st.beta_container()
+
+
+with header: 
+	st.title('application web détection')
