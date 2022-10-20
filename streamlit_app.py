ad633341a9e023922c2e9312cf83cb9c3d4e7d76
thibautcr <thibaut.cressent@gmail.com>
2022-10-20 23:50:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Application de détection")
 
 st.sidebar.title('Drop a file section')
-# st.sidebar.write('Dans un premier temps, vous devez déposer les fichiers nécessaires')
+
 st.sidebar.header("1. Upload le fichier CSV")
 df = st.sidebar.file_uploader("Deposez votre fichier au format .csv")
 st.sidebar.write("[Exemple du format de table](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
@@ -15,4 +15,8 @@ scaler = st.sidebar.file_uploader("scaler")
 
 
 st.write("df")
-st.write(df.head())
+st.dataframe(df)
+
+
+df_git = pd.read_csv("billets_production.csv")
+st.dataframe(df_git)
