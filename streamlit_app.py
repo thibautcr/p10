96f7f9dab673c38545111256e76e36c645b7d4ff
thibautcr <thibaut.cressent@gmail.com>
2022-10-20 23:39:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,15 @@ st.title("Application de détection")
 st.sidebar.title('Drop a file section')
 st.sidebar.write('Dans un premier temps, vous devez déposer les fichiers nécessaires')
 st.sidebar.header("1. Upload le fichier CSV")
-st.sidebar.write("Deposez votre fichier au format .csv")
 st.sidebar.write("Exemple du format fichier")
 # st.markdown("[![Exemple du format fichier](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
-df = st.sidebar.file_uploader()
-
+df = st.sidebar.file_uploader("Deposez votre fichier au format .csv")
+st.markdown('''
+    <a href="https://docs.streamlit.io">
+        <img src="https://media.tenor.com/images/ac3316998c5a2958f0ee8dfe577d5281/tenor.gif" />
+    </a>''',
+    unsafe_allow_html=True
+)
 
 st.sidebar.header("2. Upload des fichiers pickle")
 estimator = st.sidebar.file_uploader("estimator")
