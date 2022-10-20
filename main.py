598381262d46a5cfb7009916c7555dbccd7c937c
thibautcr <94442707+thibautcr@users.noreply.github.com>
2022-10-20 21:25:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,19 +47,31 @@ st.write("""En cliquant sur le bouton "Execute" ci-dessous, notre algorithme de 
 1. d'une part, en respectant les parametres optimaux que nous lui calculé
 2. d'autre part, en réutilisant les valeurs observées dans son entrainement préalable sur les 1200 billets du jeu d'entrainement""")
 if st.button("Execute"):
-    try:
-        file = pd.read_csv(file, sep=",", decimal=".")
-        if file.shape[1] != 7:
-            raise Exception("Le fichier n'a pas le nombre de colonnes attendues, vérifiez l'encodage (utf8; sep = ',' ; dec = '.') ou les colonnes fournies.")
-        elif not (file.columns == ['diagonal', 'height_left', 'height_right', 'margin_low', 'margin_up','length', 'id']).all():
-            raise Exception('Les colonnes entrées ne correspondent pas au schéma ci-dessus, vérifiez le nom des colonnes')
-        elif file[["id"]].duplicated().any():
-            raise Exception('Attention il y a des doublons dans la colonne "id".')
-        file.rename(columns={"id":"ide"}, inplace=True)
-    except UnicodeDecodeError:
-        st.write('Vérifiez que votre fichier est un fichier *csv* encodé en utf-8 avec comme séparateur décimal un "." et comme séparateur de valeurs une ",".')
-    except ValueError:
-        st.write("Veuillez sélectionner un fichier existant.")
+	data_test = pd.read_csv(file, sep=",", decimal=".")
+	data_index = file.id
+	data_test = scaler.transform(file.loc[:,file.columns != "id"])
+	y_pred = estimator.predict(data_test)
+	y_prob = pd.DataFrame(estimator.predict_proba(data_test).round(4))
+	results = pd.DataFrame(index = data_index)
+	results["Prédiction RegLog"] = y_pred
+	results["Probabilité d'un vrai billet"] = y_prob[1]
+	
+	
+
+# if st.button("Execute"):
+#     try:
+#         file = pd.read_csv(file, sep=",", decimal=".")
+#         if file.shape[1] != 7:
+#             raise Exception("Le fichier n'a pas le nombre de colonnes attendues, vérifiez l'encodage (utf8; sep = ',' ; dec = '.') ou les colonnes fournies.")
+#         elif not (file.columns == ['diagonal', 'height_left', 'height_right', 'margin_low', 'margin_up','length', 'id']).all():
+#             raise Exception('Les colonnes entrées ne correspondent pas au schéma ci-dessus, vérifiez le nom des colonnes')
+#         elif file[["id"]].duplicated().any():
+#             raise Exception('Attention il y a des doublons dans la colonne "id".')
+#         file.rename(columns={"id":"ide"}, inplace=True)
+#     except UnicodeDecodeError:
+#         st.write('Vérifiez que votre fichier est un fichier *csv* encodé en utf-8 avec comme séparateur décimal un "." et comme séparateur de valeurs une ",".')
+#     except ValueError:
+#         st.write("Veuillez sélectionner un fichier existant.")
 	
 #     else :
 	
