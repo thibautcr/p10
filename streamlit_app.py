643527c7807f7ba86ce0885758d0c8ab9ab692e5
thibautcr <thibaut.cressent@gmail.com>
2022-10-21 00:55:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,23 @@ st.sidebar.header("1. Upload le fichier CSV")
 file = st.sidebar.file_uploader("Deposez votre fichier au format .csv")
 st.sidebar.write("[Exemple du format de table](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
 
-st.sidebar.header("2. Upload des fichiers pickle")
-estimator = st.sidebar.file_uploader("estimator")
+# st.sidebar.header("2. Upload des fichiers pickle")
+# estimator = st.sidebar.file_uploader("estimator")
 # scaler = st.sidebar.file_uploader("scaler")
 # if estimator is not None:
 # 	estimator = pickle.loads(estimator.read())
 # if scaler is not None:
 # 	scaler = pickle.loads(scaler.read())
 
+
+
 # MAIN
+estimator = open("estimator.pkl",'rb')
+estimator = pickle.loads(estimator.read())
+scaler = open("scaler.pkl",'rb')
+estimator = pickle.loads(estimator.read())
 st.dataframe(pd.read_csv("billets_production.csv"))
+
 # if file is not None:
 # 	df = pd.read_csv(file, sep=",", decimal=".").reset_index()
 # 	df_index = df.id
