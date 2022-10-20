8c153ed66fdcc26f48b000bc4f69093c2955a880
thibautcr <thibaut.cressent@gmail.com>
2022-10-21 00:49:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,18 +13,19 @@ file = st.sidebar.file_uploader("Deposez votre fichier au format .csv")
 st.sidebar.write("[Exemple du format de table](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
 
 st.sidebar.header("2. Upload des fichiers pickle")
-estimator_pkl = st.sidebar.file_uploader("estimator")
-scaler_pkl = st.sidebar.file_uploader("scaler")
-if estimator_pkl is not None:
-	estimator = pickle.loads(estimator_pkl.read())
+estimator = st.sidebar.file_uploader("estimator")
+scaler = st.sidebar.file_uploader("scaler")
+if estimator is not None:
+	estimator = pickle.loads(estimator.read())
 if scaler_pkl is not None:
-	scaler = pickle.loads(scaler_pkl.read())
+	scaler = pickle.loads(scaler.read())
 
 # MAIN
 if file is not None:
 	df = pd.read_csv(file, sep=",", decimal=".").reset_index()
 	df_index = df.id
-	if scaler != None:
+	st.write("Fichier chargé!")
+	if scaler is not None:
 		# data_test = scaler.transform(df.loc[:,df.columns != "id"])
 		st.write("Model loaded")
 		st.write(scaler)
