1f7a81dd9b42380fe87dee8d2ad835ffd6a7e1b3
thibautcr <thibaut.cressent@gmail.com>
2022-10-20 23:57:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ st.sidebar.title('Drop a file section')
 
 st.sidebar.header("1. Upload le fichier CSV")
 file = st.sidebar.file_uploader("Deposez votre fichier au format .csv")
+# df = pd.read_csv(file, sep=",", decimal=".").reset_index()
 st.sidebar.write("[Exemple du format de table](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
 
 
@@ -13,9 +14,9 @@ st.sidebar.header("2. Upload des fichiers pickle")
 estimator = st.sidebar.file_uploader("estimator")
 scaler = st.sidebar.file_uploader("scaler")
 
-df = pd.read_csv(file, sep=",", decimal=".").reset_index()
+# df = pd.read_csv(file, sep=",", decimal=".").reset_index()
 # st.dataframe(df)
 
 
-# df_git = pd.read_csv("billets_production.csv")
-# st.dataframe(df_git)
+df_git = pd.read_csv("billets_production.csv")
+st.dataframe(df_git)
