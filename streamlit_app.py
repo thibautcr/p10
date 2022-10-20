9f8abbf7488e036cc79d53d938e75dbe1e3cf9b1
thibautcr <thibaut.cressent@gmail.com>
2022-10-20 23:41:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,8 @@ st.sidebar.header("1. Upload le fichier CSV")
 st.sidebar.write("Exemple du format fichier")
 # st.markdown("[![Exemple du format fichier](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
 df = st.sidebar.file_uploader("Deposez votre fichier au format .csv")
-st.markdown('''
-    <a href="https://docs.streamlit.io">
-        <img src="https://media.tenor.com/images/ac3316998c5a2958f0ee8dfe577d5281/tenor.gif" />
-    </a>''',
-    unsafe_allow_html=True
-)
+st.write("check out this [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
+
 
 st.sidebar.header("2. Upload des fichiers pickle")
 estimator = st.sidebar.file_uploader("estimator")
