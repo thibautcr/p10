fc4baf9d7ca880ca8724e2e94439535d479b5334
thibautcr <94442707+thibautcr@users.noreply.github.com>
2022-10-20 23:09:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+st.siderbar.title('Drop a file section')
+
+st.sidebar.header("1. Upload le fichier CSV")
+df = st.sidebar.file_uploader("Dans un premier temps, vous devez déposer votre fichier au format .csv")
+
+st.sidebar.header("2. Upload des fichiers pickle")
+estimator = st.sidebar.file_uploader("estimator")
+scaler = st.sidebar.file_uploader("scaler)
