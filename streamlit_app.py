2c3bf0f1bf57e66af322e5cdd3569ca4a12d6be1
thibautcr <thibaut.cressent@gmail.com>
2022-10-21 00:19:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,12 @@ st.title("Application de détection")
 # SIDEBAR
 st.sidebar.title('Drop a file section')
 
-st.sidebar.subheader("1. Upload le fichier CSV")
+st.sidebar.header("1. Upload le fichier CSV")
 file = st.sidebar.file_uploader("Deposez votre fichier au format .csv")
 st.sidebar.write("[Exemple du format de table](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
 
 
-st.sidebar.subheader("2. Upload des fichiers pickle")
+st.sidebar.header("2. Upload des fichiers pickle")
 estimator = st.sidebar.file_uploader("estimator")
 scaler = st.sidebar.file_uploader("scaler")
 
