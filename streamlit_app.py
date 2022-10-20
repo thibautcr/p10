ea9c4421e98b1529b2e9f30065feec071e97a0ec
thibautcr <thibaut.cressent@gmail.com>
2022-10-20 23:31:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Application de détection")
 
-st.siderbar.header('Drop a file section')
+page = st.siderbar.write('Drop a file section')
 
 # st.sidebar.header("1. Upload le fichier CSV")
 # df = st.sidebar.file_uploader("Dans un premier temps, vous devez déposer votre fichier au format .csv")
