6b8a1aa639367e2aa8acc042139189d04c99b1f0
thibautcr <thibaut.cressent@gmail.com>
2022-10-20 23:37:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 st.title("Application de détection")
 
-st.sidebar.header('Drop a file section')
+st.sidebar.title('Drop a file section')
 st.sidebar.write('Dans un premier temps, vous devez déposer les fichiers nécessaires')
 st.sidebar.header("1. Upload le fichier CSV")
 st.sidebar.write("Deposez votre fichier au format .csv")
 st.sidebar.write("Exemple du format fichier")
-st.markdown("[![Exemple du format fichier](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
+# st.markdown("[![Exemple du format fichier](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
 df = st.sidebar.file_uploader()
 
 
